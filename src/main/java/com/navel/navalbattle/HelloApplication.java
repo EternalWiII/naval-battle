79981256032328d3cb15ddff30bf4c1c459017a7
Zlata Babych <126006978+ZlataBabych@users.noreply.github.com>
2024-02-17 21:15:49 +0200

    add com //hello

--- src/main/java/com/navel/navalbattle/HelloApplication.java
+++ src/main/java/com/navel/navalbattle/HelloApplication.java
@@ -16,7 +16,7 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
         stage.show();
     }
-
+// hello
     public static void main(String[] args) {
         launch();
     }
