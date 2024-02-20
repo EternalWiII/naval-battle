dcf46c5a45bde575b42804fd71fdc722942a1d94
ArsPetr46 <137423844+ArsPetr46@users.noreply.github.com>
2024-02-20 22:02:57 +0200

    20.02: main_menu, game_menu, switching between scenes, exit

---
+++ src/main/java/com/navel/navalbattle/Main.java
@@ -0,0 +1,51 @@
+package com.navel.navalbattle;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.input.KeyCode;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main_menu.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Naval battle");
+        stage.setScene(scene);
+        stage.show();
+
+        stage.setOnCloseRequest(event -> {
+            event.consume();
+            onExitClick(stage);
+            }
+        );
+        scene.setOnKeyPressed(event -> {
+            if (event.getCode() == KeyCode.ESCAPE) {
+                Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                if (alert.showAndWait().get() == ButtonType.OK) {
+                    System.out.println("Escape pressed");
+                    stage.close();
+                }
+            }
+        });
+    }
+
+    private void onExitClick(Stage stage) {
+
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+
+        if (alert.showAndWait().get() == ButtonType.OK) {
+            stage.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/com/navel/navalbattle/MainMenuController.java
@@ -0,0 +1,63 @@
+package com.navel.navalbattle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Label;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class MainMenuController {
+
+    @FXML
+    private Button exitBtn;
+    @FXML
+    private AnchorPane scenePane;
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    @FXML
+    protected void onStartGameClick(ActionEvent e) throws IOException {
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("game.fxml")));
+        stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+        scene.setOnKeyPressed(event -> {
+            event.consume();
+            if (event.getCode() == KeyCode.ESCAPE) {
+                Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                if (alert.showAndWait().get() == ButtonType.OK) {
+                    FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main_menu.fxml"));
+                    try {
+                        Scene scene = new Scene(fxmlLoader.load());
+                        stage.setScene(scene);
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                }
+            }
+        });
+    }
+    @FXML
+    protected void onExitClick(ActionEvent event) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+
+        if (alert.showAndWait().get() == ButtonType.OK) {
+            stage = (Stage) scenePane.getScene().getWindow();
+            stage.close();
+        }
+    }
+}
